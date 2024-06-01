34c14d6a2d96470dbbd6cc667060d1332b04bbbc
geraldkisilu <muligerald6@gmail.com>
2024-06-01 15:02:52 +0300

    commit

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+import unittest
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+class TestNotNoneFunction(unittest.TestCase):
+    def test_return_not_none(self):
+        result = return_not_none()
+        self.assertIsNotNone(result, "The return_not_none function should not return None")
+
+if __name__ == '__main__':
+    unittest.main()
